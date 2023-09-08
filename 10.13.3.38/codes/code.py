623784fc85b24f2dd0fe22ec98c1551ea5f361ef
jayvik2003 <ee22btech11025@iith.ac.in>
2023-09-08 10:43:22 +0530

    A

--- 10.13.3.38/codes/code.py
+++ 10.13.3.38/codes/code.py
@@ -2,12 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import bernoulli
 
-#for no money back
+#for loss of entry fees
 #100 samples
 simlen=int(100)
 
-#One event is taking 4 balls from bag with replacement
-#Probability of the event that non of the balls are zero i.e pX(1)=(0.5)^3 which is denoted by 0 in the simulation
+#Probability of the event that non of the balls are zero i.e pX(0)=1/8 which is denoted by 1 in the simulation
 prob1 = (0.5)**3
 
 #Generating sample date using Bernoulli r.v.
@@ -22,11 +21,10 @@ print("Simulated for no money back = ",err_n1,"Theorical for no money back = ",p
 print(data_bern1)
 print("\n")
 
-#for double money back
+#for gets double entry fees
 #100 samples
 
-#One event is taking 4 balls from bag with replacement
-#Probability of the event that all tosses are heads i.e pY(1)=(0.5)^3 which is denoted by 1 in the simulation
+#Probability of the event that all tosses are heads i.e pX(3)=(0.5)^3 which is denoted by 1 in the simulation
 prob2 = (0.5)**3
 
 #Generating sample date using Bernoulli r.v.
@@ -41,11 +39,11 @@ print("Simulated for double money back = ",err_n2,"Theorical for double money ba
 print(data_bern2)
 print("\n")
 
-#for one time money back
+#for just gets the entry fees
 #100 samples
 
-#Probability of the event that all tosses are heads i.e 1-pX(1)-pY(1) which is denoted by 1 in the simulation
-prob3 = 1-prob1-prob2
+#Probability of the event that all tosses are heads i.e pX(1)+pX(2)=0.75 which is denoted by 1 in the simulation
+prob3 = 0.75
 
 #Generating sample date using Bernoulli r.v.
 data_bern3 = bernoulli.rvs(size=simlen,p=prob3)
